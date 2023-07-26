a0b4963fa6d913da2359ff1bb43ab6c28f1d9888
perry.miranda <perrymiranda1@yahoo.com>
2023-07-26 20:52:27 +0900

    auth用のserver middlewareを追加

---
+++ src/middleware/auth.global.ts
@@ -0,0 +1,28 @@
+// TODO: できればaliasを適応したい
+import { useUserMeStore } from '~/store/userMe';
+import { storeToRefs } from 'pinia';
+
+export default defineNuxtRouteMiddleware((to, from) => {
+  console.log('============================================');
+  console.log('client-side middleware: auth');
+  const userMeStore = useUserMeStore();
+  const { getUserInfo } = storeToRefs(userMeStore);
+
+  console.log('getUserInfo.value.name: ', getUserInfo.value.name);
+  console.log('getUserInfo.value.id: ', getUserInfo.value.id);
+  console.log(
+    'getUserInfo.value.accountCreatedDate: ',
+    getUserInfo.value.accountCreatedDate
+  );
+  console.log('getUserInfo.value.birthDay: ', getUserInfo.value.birthDay);
+  console.log('getUserInfo.value.birthMonth: ', getUserInfo.value.birthMonth);
+  console.log('getUserInfo.value.birthYear: ', getUserInfo.value.birthYear);
+  console.log('getUserInfo.value.email: ', getUserInfo.value.email);
+  console.log('getUserInfo.value.phoneNumber: ', getUserInfo.value.phoneNumber);
+  console.log('getUserInfo.value.role: ', getUserInfo.value.role);
+
+  const isLoggedIn = true;
+  if (!isLoggedIn) {
+    return navigateTo('/login');
+  }
+});
