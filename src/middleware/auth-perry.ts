b471824f21dfdd88bc19e3ca9bf94e766dfb7d2e
perry.miranda <perrymiranda1@yahoo.com>
2023-07-30 19:39:12 +0900

    middleware修正

--- src/middleware/auth-perry.ts
+++ src/middleware/auth-perry.ts
@@ -1,10 +1,18 @@
-export default defineNuxtRouteMiddleware((to, from) => {
+export default defineNuxtRouteMiddleware(async (to, from) => {
   console.log('============================================');
-  console.log('client-side middleware: auth');
+  console.log('client-side middleware: auth-perry');
   const { isLoggedIn } = useUserInfo();
   console.log('client-side middleware: isLoggedIn: ', isLoggedIn);
   if (!isLoggedIn.value) {
     return navigateTo('/login');
   }
+  const app = useNitroApp();
+  await app.session.get();
+
+  const { data } = await $publicApi.get('/api/session-perry');
+  console.log('session-perry data: ', data);
+
+  // return navigateTo('/login');
+
   // TODO: sessionクッキーの有効期間が切れたらログアウトするかログインページへリダイレクトするか検討
 });
