e6bd70b7ed6ed9888fa8e78b6b581885e136eb90
perry.miranda <perrymiranda1@yahoo.com>
2023-08-15 21:01:11 +0900

    login-page-redirectのmiddleware追加

---
+++ src/middleware/login-page-redirect.ts
@@ -0,0 +1,32 @@
+import { useUserMeStore } from '~/store/userMe';
+
+export default defineNuxtRouteMiddleware(async (to, from) => {
+  console.log(
+    `[${
+      process.server ? 'server' : 'client'
+    }]client-side middleware: login-page-redirect`
+  );
+  const config = useRuntimeConfig();
+  const cookie = useCookie(config.public.cookieName);
+
+  const { $publicApi } = useNuxtApp();
+  // TODO: ページ移動の時、たまに2回連続呼び出しされる、、、原因調査は必須
+  console.log(
+    `[${
+      process.server ? 'server' : 'client'
+    }]BEFORE login-page-redirect: api/session`
+  );
+  // TODO: api/sessionが二回送信、、、
+  const { data } = await $publicApi.get<ISessionGetApi>(
+    '/api/session',
+    process.server
+      ? {
+          headers: { cookie: `${config.public.cookieName}=${cookie.value}` }
+        }
+      : {}
+  );
+
+  if (!data.isRedirect) {
+    return navigateTo('/');
+  }
+});
